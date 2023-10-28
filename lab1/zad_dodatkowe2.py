e8389ff9ca8394c2c50031cc85dd3ba522b616bf
w69991 <w69991@student.wsiz.edu.pl>
2023-10-28 17:01:36 +0200

    Zadania z lab1

--- lab1/zad_dodatkowe2.py
+++ lab1/zad_dodatkowe2.py
@@ -1,1 +1,11 @@
 print("Obliczanie pola trójkąta o podanych bokach a,b,c")
+
+a = (int(input("Podaj bok a: ")))
+b = (int(input("Podaj bok b: ")))
+c = (int(input("Podaj bok c: ")))
+
+p = (a+b+c)/2
+
+P = (p*(p-a)*(p-b)*(p-c))**(1/2)
+
+print(f"Pole tego trojkata wynosi: {P}")
---
+++ lab1/zadanie_dodatkowe3.py
@@ -0,0 +1,9 @@
+print("Kalkulator dla dwoch wybranych liczb")
+
+a = (float(input("Podaj pierwsza liczbe: ")))
+b = (float(input("Podaj druga liczbe: ")))
+
+print(f"Wynik dodawania podanych liczb: {a+b}")
+print(f"Wynik odejmowania podanych liczb: {a-b}")
+print(f"Wynik mnożenia podanych liczb: {a*b}")
+print(f"Wynik dzielenia podanych liczb: {a/b}")
