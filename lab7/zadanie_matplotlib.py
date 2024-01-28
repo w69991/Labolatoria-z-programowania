1e4a4c886133deb57b841c24fed4354bdebef663
w69991 <w69991@student.wsiz.edu.pl>
2024-01-28 21:21:56 +0100

    Zadania z lab5

---
+++ lab7/zadanie_matplotlib.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+#1
+kategorie = ['Odzież', 'Elektronika', 'Książki', 'Sprzęt_sportowy']
+ilosc_sprzedanych = [320, 260, 145, 290]
+
+plt.bar(kategorie, ilosc_sprzedanych, color='green')
+
+plt.title('Ilość sprzedanych produktów w różnych kategoriach')
+plt.xlabel('Kategoria')
+plt.ylabel('Ilość sprzedanych')
+
+plt.show()
+
+#2
+plt.pie(ilosc_sprzedanych, labels=kategorie,
+autopct='%1.f%%', startangle=90,
+colors=['blue', 'green',
+'red', 'yellow'])
+plt.title('Udział w kategoriach')
+
+plt.show()
+
+#3
+czas = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+predkosc = [0, 15, 25, 30, 20, 40, 60, 80, 70]
+
+plt.scatter(czas, predkosc, color='lightblue')
+
+plt.title('Prędkość chwilowa pojazdu')
+plt.xlabel('Czas [s]')
+plt.ylabel('Prędkość [km/h]')
+
+plt.show()
+
+#4
+oceny = pd.read_csv('ostateczne')
+print(oceny)
+
+plt.hist(oceny['Ocena'], bins=5, color='purple', edgecolor='black')
+plt.title('Rozkład ocen')
+plt.xlabel('Ocena')
+plt.ylabel('Liczba wystąpień')
+
+plt.show()
+
+#5
+termin_1 = pd.read_csv('pierwszy_termin')
+termin_2 = pd.read_csv('poprawa')
+
+plt.figure(figsize=(10, 5))
+plt.plot(termin_1['Imię'], termin_1['Ocena'], marker='o', linestyle='-', color='blue', label='Termin 1')
+plt.plot(termin_2['Imię'], termin_2['Ocena'], marker='s', linestyle='--', color='green', label='Termin 2')
+plt.title('Oceny z poszczególnych terminów')
+plt.xlabel('Imię')
+plt.ylabel('Ocena')
+plt.legend()
+plt.show()
---
+++ lab7/zadanie_pandas.py
@@ -0,0 +1,63 @@
+import pandas as pd
+pierwszy_termin = {'Nr_albumu': [60001, 60005, 60020, 60007, 60042, 60023, 60201],
+ 'Imię': ['Anna', 'Jan', 'Katarzyna', 'Tomasz', 'Maciek', 'Paweł', 'Monika'],
+ 'Nazwisko': ['Jabłońska', 'Kowalski', 'Bielecka', 'Kielar', 'Szpunar', 'Kolek', 'Czerwonka'],
+ 'Wiek': [22, 21, 24, 23, 20, 25, 22],
+ 'Ocena': [4.5, 5.0, 3.5, 4.0, 2.0, 3.0, 5.0]}
+df1 = pd.DataFrame(pierwszy_termin)
+
+print(df1)
+print()
+#a
+for row in df1.itertuples():
+  if row.Ocena > 4.0:
+   print(row.Imię, row.Nazwisko, row.Ocena)
+#b
+print()
+print(df1.sort_values(['Wiek']))
+print()
+#c
+print("Średnia wieku dla osób z 5.0: ", df1.groupby('Ocena').get_group(5.0)['Wiek'].mean())
+print("Średnia wieku dla osób z 4.5: ", df1.groupby('Ocena').get_group(4.5)['Wiek'].mean())
+print("Średnia wieku dla osób z 4.0: ", df1.groupby('Ocena').get_group(4.0)['Wiek'].mean())
+print("Średnia wieku dla osób z 3.5: ", df1.groupby('Ocena').get_group(3.5)['Wiek'].mean())
+print("Średnia wieku dla osób z 3.0: ", df1.groupby('Ocena').get_group(3.0)['Wiek'].mean())
+print("Średnia wieku dla osób z 2.0: ", df1.groupby('Ocena').get_group(2.0)['Wiek'].mean())
+print()
+#d
+
+poprawa = {'Nr_albumu': [60042, 60023],
+ 'Ocena': [4.0, 5.0]}
+df2 = pd.DataFrame(poprawa)
+
+df1.set_index('Nr_albumu', inplace=True)
+df2.set_index('Nr_albumu', inplace=True)
+df1.update(df2)
+print(df1)
+print()
+#e
+df1.to_csv('oceny_z_kolokwium')
+#f
+df = pd.read_csv('oceny_z_kolokwium')
+print(df)
+print()
+#g
+nowy_uczeń = pd.DataFrame([[60013, 'Julia', 'Bar', 21, 3.5]],
+                          columns=['Nr_albumu', 'Imię', 'Nazwisko', 'Wiek', 'Ocena'])
+df = pd.concat([df, nowy_uczeń], ignore_index=True) #ignore_index żeby indexy zostały zresetowane
+print(df)
+print()
+#h
+unikalne_wartości = df['Ocena'].unique()
+print("Unikalne oceny to:", (unikalne_wartości))
+#i
+oceny_5 = df['Ocena'].value_counts().get(5.0, 0)
+print("Liczba uczniów z ocenami 5.0: ", oceny_5)
+
+#do zadania z matplotlib
+df1.to_csv('poprawa')
+df.set_index('Nr_albumu', inplace=True)
+df.to_csv('ostateczne')
+df0 = pd.DataFrame(pierwszy_termin)
+df0.set_index('Nr_albumu', inplace=True)
+df0.to_csv('pierwszy_termin')
